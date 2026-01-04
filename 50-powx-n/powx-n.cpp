ac72c4c309481b34a6d3d95bea7821fda0ffe7fe
swapnil <swapnilambad80@gmail.com>
2026-01-04 16:50:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (42.83%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double f(double x, int n){
+        if(x == 0) return 0;
+        if(n == 0) return 1;
+        if(n == 1) return x;
+        if(n == -1) return 1/x;
+        double prod = f(x, n/2);
+        if(n < 0 && n%2) return (1/x)*prod*prod;
+        else if(n%2) return x*prod*prod;
+        return prod*prod;
+    }
+    double myPow(double x, int n) {
+        return f(x, n);
+    }
+};
