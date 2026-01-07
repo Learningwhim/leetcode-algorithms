acf39279f2836d4bf3421deeb96ff517df7ee089
swapnil <swapnilambad80@gmail.com>
2026-01-08 01:43:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (70.45%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
-    double f(double x, int n){
-        if(x == 0) return 0;
-        if(n == 0) return 1;
-        if(n == 1) return x;
-        if(n == -1) return 1/x;
-        double prod = f(x, n/2);
-        if(n < 0) x = 1/x;
-        if(n%2) return x*prod*prod;
-        return prod*prod;
-    }
-    double myPow(double x, int n) {
-        return f(x, n);
+    // double f(double x, int n){
+    //     if(x == 0) return 0;
+    //     if(n == 0) return 1;
+    //     if(n == 1) return x;
+    //     if(n == -1) return 1/x;
+    //     double prod = f(x, n/2);
+    //     if(n < 0) x = 1/x;
+    //     if(n%2) return x*prod*prod;
+    //     return prod*prod;
+    // }
+    double myPow(double x, long long n) {
+        double ans = 1;
+        long long m = n;
+        n = abs(1ll*n);
+        while(n > 0){
+            if(n%2){
+                ans = ans*x;
+                n = n-1;
+            }else{
+                n = n/2;
+                x = x*x;    
+            }
+        }
+        if(m < 0) return 1.0/ans;
+        return ans;
     }
 };
