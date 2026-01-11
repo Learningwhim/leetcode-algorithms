5e187a087ddd42c18b52eb100cf9dca2f7a7baf2
swapnil <swapnilambad80@gmail.com>
2026-01-12 04:55:34 +0530

    Time: 34 ms (46.75%) | Memory: 94.2 MB (5.43%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,19 +1,20 @@
 class StockSpanner {
 public:
     vector<int> arr;
+    stack<int> st;
     StockSpanner() {
             arr = vector<int> ();
-        
+            st = stack<int> ();
     };
     
     int next(int price){
+        int ans = 1;
             arr.push_back(price);
-            int cnt = 1;
-            for(int i = arr.size()-2; i >= 0; --i){
-                if(arr[i] <= price) cnt++;
-                else break;
-            }
-            return cnt;
+            while(!st.empty() && arr[st.top()] <= price) st.pop();
+            if(!st.empty()) ans = arr.size()-st.top()-1;
+            else ans = arr.size();
+            st.push(arr.size()-1);
+            return ans;
         }
 };
 
