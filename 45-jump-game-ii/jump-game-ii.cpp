49cd65e6390b73287594a4ff1b5328a6b5d4fe47
swapnil <swapnilambad80@gmail.com>
2026-01-17 00:57:25 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (11.37%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int curr = nums[0], jumpct = 0;
+        int curr = 0, jumpct = 0, farthest = 0;
         int n = nums.size();
         vector<int> dp(n, 1e9);
         dp[0] = 0;
-        for(int i = 0; i < n; ++i){
-            for(int j = i+1; j < n && j <= i+nums[i]; ++j){
-                dp[j] = min(dp[j], dp[i]+1);
+        for(int i = 0; i < n-1; ++i){
+            farthest = max(farthest, i+nums[i]);
+            if(i == curr){
+                curr = farthest;
+                jumpct++;
             }
         }
-        return dp[n-1];
+        return jumpct;
     }
 };
