967f297aa53794360b002021b141fd990dc31038
swapnil <swapnilambad80@gmail.com>
2026-01-25 20:02:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (13.65%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -20,7 +20,8 @@ public:
     }
     int maxDepth(TreeNode* root) {
         if(root == NULL) return 0;
-        md(maxd, root);
-        return maxd;
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+        return 1 + max(lh, rh);
     }
 };
