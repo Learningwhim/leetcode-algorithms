79d2b6048872215eb08fb3e3c3d41b12bbf7e976
swapnil <swapnilambad80@gmail.com>
2026-01-26 15:06:53 +0530

    Time: 16 ms (47.69%) | Memory: 36.7 MB (64.03%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -6,12 +6,12 @@ public:
         int mind = INT_MAX;
         int n = arr.size();
         for(int i = 1; i < n; ++i){
-            if(arr[i]-arr[i-1] < mind){
-                ans = vector<vector<int>>();
+             if(arr[i]-arr[i-1] < mind){
                 mind = arr[i]-arr[i-1];
-                ans.push_back({arr[i-1], arr[i]});
-
-            }else if(arr[i]-arr[i-1] == mind) ans.push_back({arr[i-1], arr[i]});
+             }
+        }
+        for(int i = 1; i < n; ++i){
+            if(arr[i]-arr[i-1] == mind) ans.push_back({arr[i-1], arr[i]});
         }
         return ans;
     }
