87bae2ebd8d7e026759f21bc1bb60c2faafdb0e0
swapnil <swapnilambad80@gmail.com>
2026-02-28 13:49:53 +0530

    Time: 4 ms (15.59%) | Memory: 17.3 MB (12.62%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<pair<int , int>, int>> q;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+        int time = 0;
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < m; ++j){
+                if(grid[i][j] == 2){
+                    q.push({{i, j}, 0});
+                }
+            }
+        }
+        while(!q.empty()){
+            int sz = q.size();
+            for(int k = 0; k < sz; ++k){
+                auto curr = q.front();
+                q.pop();
+                int x = curr.first.first, y = curr.first.second;
+                time = max(curr.second, time);
+                if(x < n-1 && !vis[x+1][y] && grid[x+1][y] == 1){
+                    vis[x+1][y] = true;
+                    q.push({{x+1,y}, curr.second+1});
+                }
+                if(x > 0 && !vis[x-1][y] && grid[x-1][y] == 1){
+                    vis[x-1][y] = true;
+                    q.push({{x-1,y}, curr.second+1});
+                }
+                if(y < m-1 && !vis[x][y+1] && grid[x][y+1] == 1){
+                    vis[x][y+1] = true;
+                    q.push({{x,y+1}, curr.second+1});
+                }
+                if(y > 0 && !vis[x][y-1] && grid[x][y-1] == 1){
+                    vis[x][y-1] = true;
+                    q.push({{x,y-1}, curr.second+1});
+                }
+            }
+
+        }
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < m; ++j){
+                if(grid[i][j] == 1 && !vis[i][j]){
+                    return -1;
+                }
+            }
+        }
+        return time;
+    }
+};
