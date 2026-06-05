18d031456bb150994f2c6e9f8c9b12b6a0c6c355
swapnil <swapnilambad80@gmail.com>
2026-06-05 17:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (72.63%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,32 +1,64 @@
 class Solution {
 public:
-    
-    void dfs(int node,vector<bool> &vis, vector<vector<int>> &graph){
-        if(vis[node]) return;
-        vis[node] = true;
-        for(auto adj : graph[node]){
-            dfs(adj, vis, graph);
+    class DisjointSet{
+        public:
+        vector<int> rank, parent, size;
+
+        DisjointSet(int n){
+            rank.resize(n, 0);
+            size.resize(n, 1);
+            parent.resize(n);
+            for(int i = 0; i < n; ++i) parent[i] = i;
         }
-    }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        
-        vector<bool> vis;
-        vector<vector<int>> graph;
-        int n = isConnected.size();
-        vis.assign(n, false);
-        graph.assign(n, vector<int>());
-        for(int i = 0; i < n; ++i){
-            for(int j = 0; j < n; ++j){
-                
-                if(isConnected[i][j] == 1) graph[i].push_back(j);
+
+        int findUpar(int node){
+            if(node == parent[node]) return node;
+            return parent[node] = findUpar(parent[node]);
+        }
+
+        void unionByRank(int u, int v){
+            int ulp_u = findUpar(u);
+            int ulp_v = findUpar(v);
+            if(ulp_v == ulp_u) return;
+            if(rank[ulp_u] < rank[ulp_v]){
+                parent[ulp_u] = ulp_v;
+            }else if(rank[ulp_v] < rank[ulp_u]){
+                parent[ulp_v] = ulp_u;
+            }else{
+                parent[ulp_u] = ulp_v;
+                rank[ulp_v]++;
             }
         }
-        int count = 0;
-        for(int i = 0; i < n; ++i){ // we will try dfs from each node
-            if(vis[i]) continue;
-            count++;
-            dfs(i, vis, graph);
+
+        void unionBySize(int u, int v){
+            int ulp_u = findUpar(u);
+            int ulp_v = findUpar(v);
+            if(ulp_v == ulp_u) return;
+            if(size[ulp_u] < size[ulp_v]){
+                parent[ulp_u] = ulp_v;
+                size[ulp_v] += size[ulp_u];
+            }else if(size[ulp_v] < size[ulp_u]){
+                parent[ulp_v] = ulp_u;
+                size[ulp_u] += size[ulp_v];
+            }else{
+                parent[ulp_u] = ulp_v;
+                size[ulp_v] += size[ulp_u];
+            }
         }
-        return count;
+    };
+    int findCircleNum(vector<vector<int>>& adj) {
+        int n = adj.size();
+        DisjointSet ds(n+1);
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < n; ++j){
+                if(adj[i][j]) ds.unionByRank(i, j);
+            }
+        }
+        int cnt = 0;
+        for(int i = 0; i < n; ++i){
+            if(ds.findUpar(i) == i) cnt++;
+        }
+        return cnt;
+
     }
 };
