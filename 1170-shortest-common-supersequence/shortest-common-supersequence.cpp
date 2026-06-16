3f5c0589f4f32662e8cc8c970ada52a5cb1ff4d6
swapnil <swapnilambad80@gmail.com>
2026-06-16 17:36:23 +0530

    Time: 27 ms (37.58%) | Memory: 26.7 MB (89.27%) - LeetSync

---
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    string f(string s1, string s2){
+        string ans = "";
+        int n = s1.size(), m = s2.size();
+        int i = n, j = m;
+        while(i > 0 && j > 0){
+            if(s1[i-1] == s2[j-1]){
+                ans.push_back(s1[i-1]);
+                i--;j--;
+            }else if(dp[i-1][j] < dp[i][j-1]){
+                ans.push_back(s2[j-1]);
+                j--;
+            }else{
+                ans.push_back(s1[i-1]);
+                i--;
+            }
+        }
+        while(i > 0) ans.push_back(s1[--i]);
+        while(j > 0) ans.push_back(s2[--j]);
+        return ans;
+    }
+    string shortestCommonSupersequence(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
+        dp.assign(n+1, vector<int> (m+1, 0));
+        for(int i = 1; i <= n; ++i){
+            for(int j = 1; j <= m; ++j){
+                if(s1[i-1] == s2[j-1]) dp[i][j] = 1+dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }  
+        int length = n+m-dp[n-1][m-1];
+        string ans = f(s1, s2);
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
