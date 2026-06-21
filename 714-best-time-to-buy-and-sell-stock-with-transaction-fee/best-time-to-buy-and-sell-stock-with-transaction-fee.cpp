afae875f8b826b0ccce45209df6787f43a18dc6d
swapnil <swapnilambad80@gmail.com>
2026-06-21 22:46:33 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (90.16%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int f(int i, int can, vector<int>& prices, int fee){
-        if(i == prices.size()) return 0;
-        if(dp[i][can] != -1) return dp[i][can];
-        if(can){
-            int buy = -prices[i] + f(i+1, 0, prices, fee);
-            int skip = f(i+1, 1, prices, fee);
-            return dp[i][can] = max(buy, skip);
-        }else{
-            int sell = prices[i]-fee + f(i+1, 1, prices, fee);
-            int hold = f(i+1, 0, prices, fee);
-            return dp[i][can] = max(sell, hold);
-        }
-    }
+    
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
 
-        dp.assign(n+1, vector<int> (2, -1));
-        return f(0, 1, prices, fee);
+        int buy = INT_MIN;
+        int sell = 0;
+        for(int i = 0; i < n; ++i){
+            buy = max(buy, sell-prices[i]);
+            sell = max(sell, buy+prices[i]-fee);
+        }
+        return sell;
     }
 };
