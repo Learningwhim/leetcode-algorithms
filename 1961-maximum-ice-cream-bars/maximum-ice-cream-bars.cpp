b25c7108a85f4c47a679487728bf656c6dffd774
swapnil <swapnilambad80@gmail.com>
2026-06-21 18:33:01 +0530

    Time: 35 ms (49.96%) | Memory: 80.3 MB (37.51%) - LeetSync

---
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(), costs.end());
+        int ans = 0;
+        int n = costs.size();
+        for(int i = 0;  i < n; ++i){
+            if(coins >= costs[i]) {
+                coins -= costs[i];
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
