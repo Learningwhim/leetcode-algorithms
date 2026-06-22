79212543456ed3df066e31360689780f869b3948
swapnil <swapnilambad80@gmail.com>
2026-06-22 18:04:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (39.61%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+
+    int maxNumberOfBalloons(string s) {
+        int n = s.size();
+        unordered_map<char, int> mp;
+        for(int i = 0; i  < n; ++i) mp[s[i]]++;
+        
+        return min({mp['b'], mp['a'], mp['l']/2, mp['o']/2, mp['n']});
+
+
+    }
+};
