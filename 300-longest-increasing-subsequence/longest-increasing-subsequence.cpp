36d1967f48e619bc452fae155d13be060304ae88
swapnil <swapnilambad80@gmail.com>
2026-06-23 20:42:42 +0530

    Time: 281 ms (29.40%) | Memory: 296.4 MB (24.88%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -13,7 +13,18 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        dp.assign(n+1, vector<int>(n+1, -1));
-        return f(0, -1, nums);
+        dp.assign(n+1, vector<int>(n+1, 0));
+        //return f(0, -1, nums);
+        for(int i = n-1; i >= 0; --i){
+            for(int j = i-1; j >= -1; --j){
+                int skip = dp[i+1][j+1] ;
+                int take = 0;
+                if (j == -1 || nums[i] > nums[j]) {
+                    take = 1 + dp[i+1][i+1];
+                }
+                dp[i][j+1] = max(take, skip); 
+            }
+        }
+        return dp[0][0];
     }
 };
