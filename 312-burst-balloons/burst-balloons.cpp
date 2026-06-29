2394e1d72cd5a07b9779e63a72ea8d4f76240c0f
swapnil <swapnilambad80@gmail.com>
2026-06-30 01:30:22 +0530

    Time: 99 ms (72.18%) | Memory: 13.8 MB (81.96%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -11,13 +11,34 @@ public:
         }
         return dp[i][j] = maxi;
     }
-    int maxCoins(vector<int>& nums) {
-        vector<int> arr;
-        arr.push_back(1);
-        for(int i = 0; i < nums.size(); ++i) arr.push_back(nums[i]);
-        arr.push_back(1);
-        int n = nums.size();
-        dp.assign(n+2, vector<int> (n+2, -1));
-        return f(1, nums.size(), arr);
+    int maxCoins(vector<int>& arr) {
+        vector<int> nums;
+        nums.push_back(1);
+        for(int i = 0; i < arr.size(); ++i) nums.push_back(arr[i]);
+        nums.push_back(1);
+        
+        int n = arr.size();
+        dp.assign(n+2, vector<int> (n+2, 0));
+        if(n==1)
+        {
+            return arr[0];
+        }
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(i>j)
+                {
+                    continue;
+                }
+                int maxi=0;
+                for(int k=i;k<=j;k++)
+                {
+                    maxi=max(maxi,nums[i-1]*nums[k]*nums[j+1]+dp[i][k-1]+dp[k+1][j]);
+                }
+                dp[i][j]=maxi;
+            }
+        }
+        return dp[1][n];
     }
 };
